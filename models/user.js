fbc2079e15402a43d114ac3960b13089834d1e74
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-11-19 18:45:08 +0530

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+// Some changes need to be made in the schema ( make in unique and regex will be applied )
+const userSchema = new Schema({
+  name: { type: "string", required: true },
+  dob: { type: "string", required: true },
+  regNo: { type: "string", required: true },
+});
+
+module.exports = userSchema;
